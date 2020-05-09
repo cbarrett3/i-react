f411acf72b904188a1dcef3583380cba10170a9f
Connor Barrett <connor.steven.barrett@gmail.com>
2020-05-08 21:40:18 -0500

    details on CreatePost

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -38,7 +38,7 @@ class CreatePost extends Component {
     const { content, priv_post } = this.state
     return (
       <form onSubmit={this.handleSubmit}>
-        <div className="flex flex-column ph3 mb2 helvetica">
+        <div className="flex flex-column ph3 mb1 helvetica">
           <div class="flex mb1">
             <textarea 
               id="post"
@@ -58,34 +58,34 @@ class CreatePost extends Component {
                 class="br-pill h2-m w2-m h2 w2" alt="avatar">
             </img>
           </div>
-          <div class="flex justify-between">
-            <div class="measure pr5">
+          <div class="flex justify-between mb1">
+            <div class="measure pr1">
               {/* <label for="name" class="f6 b db mb2">Add Tags <span class="normal black-60">(optional)</span></label> */}
               <input id="name" class="input-reset w-100 ba b--white pv1" type="text" placeholder="Add Tags" aria-describedby="name-desc"/>
               {/* <small id="name-desc" class="f6 black-60 db mb2">Helper text for the form control.</small> */}
             </div>
               <label>
                 {/* Audience: */}
-                <select priv_post={this.state.priv_post} onChange={this.handleChange}>
-                  <option selected priv_post={true}>Private</option>
-                  <option priv_post={false}>Public</option>
+                <select priv_post={this.state.priv_post} class="select-css" onChange={this.handleChange}>
+                  <option selected priv_post={true}>Private </option>
+                  <option priv_post={false}>Public </option>
                 </select>
               </label>
           </div>
-          <div class="flex justify-between pv3">
+          <div class="flex justify-between mt2">
             <div>
-              <a className='f5 link dim br-pill pv1 ph2 mr2 dib white bg-green helvetica' href='#0'>
+              <a className='f5 link dim br-pill pv2 ph3 mr1 mt1 dib white bg-green helvetica' href='#0'>
                 <b>x </b>
                 Added Tag
               </a>
-              <a className='f5 link dim br-pill pv1 ph2 mr2 dib white bg-green helvetica' href='#0'>
+              <a className='f5 link dim br-pill pv2 ph3 mr1 mt1 dib white bg-green helvetica' href='#0'>
                 <b>x </b>
                 Added Tag 2
               </a>
             </div>
             <div>
               <Mutation mutation={CREATE_POST_MUTATION} variables={{ content, priv_post }}>
-                {createPostMutation => <a  className='f5 link dim br2 ph3 pv2 white bg-pink helvetica' href='#0' onClick={createPostMutation}>Post</a>}
+                {createPostMutation => <a  className='f4 link dim br-pill ph3 pv2 white bg-pink helvetica' href='#0' onClick={createPostMutation}>Post</a>}
               </Mutation>
             </div>
           </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,28 +1,28 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import { withRouter } from 'react-router'
-import juice from '../images/watermelon.svg'; 
+import logo from '../images/juice-logo.svg'; 
 
 
 
 class Header extends Component {
   render() {
     return (
-      <div className="flex f4 justify-around nowrap gray helvetica">
-        <Link to="/" className="App-logo mr3 mt3 b no-underline pink">
+      <div className="flex f4 mt3 justify-around nowrap gray helvetica">
+        <Link to="/" className="App-logo mr3 b no-underline pink">
             home
             {/* <img src={home} class="mr2" alt="juicelogo"></img> */}
         </Link>
-        <div className="fw7 mr2 mt3">
+        <div className="fw7 mr2">
             profile
         </div>
         <div class="mw-10">
-            <img src={juice} class="mr2 mt2" alt="juicelogo"></img>
+            <img src={logo} class="mr2" alt="juicelogo"></img>
         </div>
-        <div className="fw7 mr2 mt3">
+        <div className="fw7 mr2">
             notifications
         </div>
-        <div className="fw7 mt3">
+        <div className="fw7">
             more
         </div>
       </div>
